7912bef709a23cb712c5b600b3477b2d5e478c09
김봉준 <nocdu112@naver.com>
2023-11-16 19:20:11 +0900

    redux store의 직렬화 체크 해제

--- src/app/store.ts
+++ src/app/store.ts
@@ -24,7 +24,7 @@ export const store = configureStore({
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST'],
+        ignoredActions: ['persist/PERSIST', 'persist/PURGE'],
       },
     }),
 });
