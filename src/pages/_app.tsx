884d7a2a1d7ea8016a0bad8607177692fbad9388
김봉준 <nocdu112@naver.com>
2023-11-16 16:07:29 +0900

    redux-persist 설정 및 전역함수 설정 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,28 +5,22 @@ import { QueryParamProvider } from 'use-query-params';
 import NextAdapterPages from 'next-query-params/pages';
 import { theme } from '../theme';
 import { Provider } from 'react-redux';
-import store from '../app/store';
+import { store, persistor } from '../app/store'; // `store`와 `persistor`를 불러옵니다
+import { PersistGate } from 'redux-persist/integration/react'; // PersistGate를 불러옵니다
+import GlobalFunctionHandler from '../features/global/GlobalFunctionHandler';
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    console.log('_app.tsx useEffect');
-    //전역 JavaScript 함수를 window 객체에 바인딩
-    (window as any).fireAgency = {
-      //TODO 네이티브 앱으로부터 메시지를 받는 로직 구현
-      // postMessage: (message: any) => {
-      //   console.log('Received message:', message);
-        
-      // },
-    };
-  }, []);
   return (
     <Provider store={store}>
-      <ChakraProvider theme={theme} resetCSS>
-        <QueryParamProvider adapter={NextAdapterPages}>
-          <Component {...pageProps} />
-        </QueryParamProvider>
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme} resetCSS>
+          <QueryParamProvider adapter={NextAdapterPages}>
+            <GlobalFunctionHandler />
+            <Component {...pageProps} />
+          </QueryParamProvider>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   );
 }
