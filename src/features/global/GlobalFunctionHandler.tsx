a29e98deffcf3d563a24066d2a5633bbbc67a6ca
김봉준 <nocdu112@naver.com>
2023-11-16 16:05:21 +0900

    전역 동작 함수 추가(앱의 위치좌표, gps 상태)

---
+++ src/features/global/GlobalFunctionHandler.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { saveUserLocationX, saveUserLocationY, saveGpsStatusSatelliteCount, saveGpsStatusDbHzAverage } from '../../features/slice/UserinfoSlice';
+
+const GlobalFunctionHandler = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    window.setGpsStatus = (satelliteCount: number, dbHzAverage: number) => {
+        //console.log(`satelliteCount is ${satelliteCount} dbHzAverage is ${dbHzAverage}`)
+        dispatch(saveGpsStatusSatelliteCount(satelliteCount));
+        dispatch(saveGpsStatusDbHzAverage(dbHzAverage));
+    };
+
+    window.updateLocation = (latitude: number, longitude: number) => {
+        console.log(`location latitude=${latitude} longitude=${longitude}`)
+        dispatch(saveUserLocationX(latitude));
+        dispatch(saveUserLocationY(longitude));
+    };
+  }, [dispatch]);
+
+  return null;
+};
+
+export default GlobalFunctionHandler;
