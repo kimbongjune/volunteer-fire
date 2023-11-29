68c60481fa06713f20b86ca0330f0f9854a9bffc
김봉준 <nocdu112@naver.com>
2023-11-30 00:16:44 +0900

    axios 인스턴스 생성

--- src/common/components/api/axios.ts
+++ src/common/components/api/axios.ts
@@ -2,15 +2,17 @@ import axios from 'axios';
 
 // Axios 인스턴스를 생성
 const axiosInstance = axios.create({
-  baseURL: 'https://run.mocky.io/v3',
+  baseURL: 'http://121.152.148.227:18080/volfire-1.0.0-BUILD-SNAPSHOT',
+  //baseURL: 'http://192.168.10.210:8080/volfire-1.0.0-BUILD-SNAPSHOT',
+  //baseURL: 'http://localhost:8080/',
 });
 
-// 요청 인터셉터를 추가하여 모든 요청에 토큰을 포함
 axiosInstance.interceptors.request.use((config) => {
   //TODO 앱의 room db에서 jwt 토큰을 가져와서 할당.
   const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  console.log(token)
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers['Authorization'] = `${token}`;
   }
   return config;
 }, (error) => {
@@ -23,7 +25,7 @@ export const setAuthToken = (token:String) => {
     console.log("setAuthToken", token);
     if (token) {
       // 토큰이 있는 경우, 모든 요청의 헤더에 Authorization을 설정합니다.
-      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      axiosInstance.defaults.headers.common['Authorization'] = `${token}`;
     } else {
       // 토큰이 없는 경우, Authorization 헤더를 삭제합니다.
       delete axiosInstance.defaults.headers.common['Authorization'];
