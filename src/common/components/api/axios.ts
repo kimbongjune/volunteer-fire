bf1923c470b94b6b7a96bd1103d1b920a8631815
김봉준 <nocdu112@naver.com>
2023-12-05 20:31:38 +0900

    axios 개발 서버로 변경

--- src/common/components/api/axios.ts
+++ src/common/components/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Axios 인스턴스를 생성
 const axiosInstance = axios.create({
   //baseURL: 'http://112.25.1.94:8098/volunteer/',
-  baseURL: 'http://192.168.10.210:8080/volfire-1.0.0-BUILD-SNAPSHOT',
+  baseURL: 'http://121.152.148.227:18080/volfire-1.0.0-BUILD-SNAPSHOT',
   //baseURL: 'http://localhost:8080/',
 });
 
@@ -22,7 +22,7 @@ axiosInstance.interceptors.request.use((config) => {
 
 // 토큰을 동적으로 axios 헤더에 추가하는 함수
 export const setAuthToken = (token:String) => {
-    console.log("setAuthToken", token);
+    //console.log("setAuthToken", token);
     if (token) {
       // 토큰이 있는 경우, 모든 요청의 헤더에 Authorization을 설정합니다.
       axiosInstance.defaults.headers.common['Authorization'] = `${token}`;
