5f6a074902222bd69907631e4592198fe4df910e
kimbongjune <nocdu112@naver.com>
2023-12-26 17:00:55 +0900

    cpr 영상 변경 및 로직 작성

--- src/features/Cpr/CPR.tsx
+++ src/features/Cpr/CPR.tsx
@@ -28,17 +28,32 @@ const CPR = () => {
     sendClickStream()
   }, [userInfo])
 
+  // useEffect(() => {
+  //   setVideos([
+  //     { title: 'CRP-성인', date: '2023.12.18', playTime: '1분', video: "/videos/cpr_adult.mp4" },
+  //     { title: 'CPR-소아', date: '2023.09.09', playTime: '1분 4초', video: "/videos/cpr_child.mp4" },
+  //     { title: 'CPR-유아', date: '2023.09.09', playTime: '1분 2초', video: "/videos/cpr_baby.mp4" },
+  //     { title: '하임리히-성인', date: '2023.09.09', playTime: '23초', video: "/videos/heimlich_adult.mp4" },
+  //     { title: '하임리히-소아', date: '2023.09.09', playTime: '15초', video: "/videos/heimlich_child.mp4" },
+  //     { title: '하임리히-영아', date: '2023.09.09', playTime: '17초', video: "/videos/heimlich_baby.mp4"},
+  //     { title: '자세변경-호흡곤란', date: '2023.09.09', playTime: '35초', video: "/videos/change_posture_difficulty_breath.mp4"},
+  //     { title: '자세변경-복통', date: '2023.09.09', playTime: '30초', video: "/videos/change_posture_stomachache.mp4" },
+  //     { title: '자세변경-좌측위', date: '2023.09.09', playTime: '26초', video: "/videos/change_posture_left_up.mp4" },
+  //   ]);
+  // }, []);
+
   useEffect(() => {
     setVideos([
-      { title: 'CRP-성인', date: '2023.09.08', playTime: '1분', video: "/videos/cpr_adult.mp4" },
-      { title: 'CPR-소아', date: '2023.09.09', playTime: '1분 4초', video: "/videos/cpr_child.mp4" },
-      { title: 'CPR-유아', date: '2023.09.09', playTime: '1분 2초', video: "/videos/cpr_baby.mp4" },
-      { title: '하임리히-성인', date: '2023.09.09', playTime: '23초', video: "/videos/heimlich_adult.mp4" },
-      { title: '하임리히-소아', date: '2023.09.09', playTime: '15초', video: "/videos/heimlich_child.mp4" },
-      { title: '하임리히-영아', date: '2023.09.09', playTime: '17초', video: "/videos/heimlich_baby.mp4"},
-      { title: '자세변경-호흡곤란', date: '2023.09.09', playTime: '35초', video: "/videos/change_posture_difficulty_breath.mp4"},
-      { title: '자세변경-복통', date: '2023.09.09', playTime: '30초', video: "/videos/change_posture_stomachache.mp4" },
-      { title: '자세변경-좌측위', date: '2023.09.09', playTime: '26초', video: "/videos/change_posture_left_up.mp4" },
+      { title: 'AED 사용법', date: '2023.12.18', playTime: '52초', video: "/videos/aed_use.mp4" },
+      { title: 'CPR-성인', date: '2023.12.18', playTime: '1분 46초', video: "/videos/cpr_adult.mp4" },
+      { title: 'CPR-소아', date: '2023.12.18', playTime: '1분 48초', video: "/videos/cpr_child.mp4" },
+      { title: 'CPR-유아', date: '2023.12.18', playTime: '1분 47초', video: "/videos/cpr_baby.mp4" },
+      { title: '성인소아-기도폐쇄', date: '2023.12.18', playTime: '48초', video: "/videos/heimlich_adult_child.mp4" },
+      { title: '영아-기도폐쇄', date: '2023.12.18', playTime: '1분 30초', video: "/videos/heimlich_baby.mp4"},
+      { title: '성인소아-경련중', date: '2023.12.18', playTime: '42초', video: "/videos/convulsions_adult_child.mp4"},
+      { title: '영아-경련중', date: '2023.12.18', playTime: '31초', video: "/videos/convulsions_baby.mp4"},
+      { title: '성인소아-의식저하', date: '2023.12.18', playTime: '34초', video: "/videos/consciousness_adult_child.mp4" },
+      { title: '영아-의식저하', date: '2023.12.18', playTime: '29초', video: "/videos/consciousness_baby.mp4" },
     ]);
   }, []);
 
